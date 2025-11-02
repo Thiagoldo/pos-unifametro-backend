9487f28d7f8d8b28d1d3d7f2e8709ca99a9160df
Thiago Lima <thiagolimadeoliveira18@gmail.com>
2025-11-02 20:10:17 -0300

    feat: enable nginx and update microsservices routes

--- microservices/chat-service/app/__init__.py
+++ microservices/chat-service/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from .config import Config
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 mongo = PyMongo()
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
     app.config.from_object(Config)
     
     mongo.init_app(app)
@@ -16,7 +18,7 @@ def create_app():
         
         # Register Blueprints
         from flask_restx import Api
-        api = Api(app, title='Chat API', version='1.0', description='API for chat management')
+        api = Api(app, title='Chat API', version='1.0', description='API for chat management', doc='/doc')
         api.add_namespace(routes.chat_bp)
 
         return app
--- microservices/users-service/app/__init__.py
+++ microservices/users-service/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 db = SQLAlchemy()
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
     app.config.from_object(Config)
     
     db.init_app(app)
@@ -16,7 +18,7 @@ def create_app():
         
         # Register Blueprints
         from flask_restx import Api
-        api = Api(app, title='Users API', version='1.0', description='API for user management')
+        api = Api(app, title='Users API', version='1.0', description='API for user management', doc='/doc')
         api.add_namespace(routes.users_bp)
 
         # Create database tables for our models
